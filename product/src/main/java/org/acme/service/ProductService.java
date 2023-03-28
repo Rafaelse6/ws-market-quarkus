3bb902ce2f07c2672d9e5469f1ef06316590086b
Rafael Santos <rafa.05125@gmail.com>
2023-03-28 01:42:36 -0300

    Product service

---
+++ product/src/main/java/org/acme/service/ProductService.java
@@ -0,0 +1,71 @@
+package org.acme.service;
+
+import org.acme.dto.ProductDTO;
+import org.acme.entity.ProductEntity;
+import org.acme.repository.ProductRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+
+@ApplicationScoped
+public class ProductService {
+
+    @Inject
+    private ProductRepository productRepository;
+
+    public List<ProductDTO> findAllProducts(){
+        List<ProductDTO> products = new ArrayList<>();
+
+        productRepository.findAll().stream().forEach(item ->{
+            products.add(mapProductEntityToDTO(item));
+        });
+
+        return products;
+    }
+
+    public void createNewProduct(ProductDTO productDTO){
+        productRepository.persist(mapProductDtoToEntity(productDTO));
+    }
+
+    public void changeProduct(Long id, ProductDTO product){
+        ProductEntity productEntity = productRepository.findById(id);
+
+        productEntity.setName(product.getName());
+        productEntity.setDescription(product.getDescription());
+        productEntity.setCategory(product.getCategory());
+        productEntity.setModel(product.getModel());
+        productEntity.setPrice(product.getPrice());
+
+        productRepository.persist(productEntity);
+    }
+
+    public void deleteProduct(Long id){
+        productRepository.deleteById(id);
+    }
+
+    private ProductDTO mapProductEntityToDTO(ProductEntity product){
+        ProductDTO productDTO = new ProductDTO();
+
+        productDTO.setName(product.getName());
+        productDTO.setDescription(product.getDescription());
+        productDTO.setCategory(product.getCategory());
+        productDTO.setModel(product.getModel());
+        productDTO.setPrice(product.getPrice());
+
+        return productDTO;
+    }
+
+    private ProductEntity mapProductDtoToEntity(ProductDTO productDTO){
+        ProductEntity product = new ProductEntity();
+
+        product.setName(productDTO.getName());
+        product.setDescription(productDTO.getDescription());
+        product.setCategory(productDTO.getCategory());
+        product.setModel(productDTO.getModel());
+        product.setPrice(productDTO.getPrice());
+
+        return product;
+    }
+}
