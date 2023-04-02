773f70726e45c8da2da6d8994d0bb725911d5cd1
Rafael Santos <rafa.05125@gmail.com>
2023-04-02 01:17:31 -0300

    Order entity and dto

---
+++ order/src/main/java/org/acme/dto/OrderDTO.java
@@ -0,0 +1,23 @@
+package org.acme.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.jackson.Jacksonized;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Jacksonized
+public class OrderDTO {
+
+    private Long customerId;
+
+    private String customerName;
+
+    private Long productId;
+    
+    private BigDecimal orderValue;
+}
---
+++ order/src/main/java/org/acme/entity/OrderEntity.java
@@ -0,0 +1,31 @@
+package org.acme.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name="orderProducts")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderEntity {
+
+    @Id
+    @GeneratedValue
+    private Long orderId;
+
+    private String customerId;
+
+    private String customerName;
+
+    private Long productId;
+
+    private BigDecimal orderValue;
+}
